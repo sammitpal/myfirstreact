d1acb36ac4f58a54a6c42cc3df7fe4e561e3ff83
samitpal2000 <sammit.dp.pal2000@gmail.com>
2020-07-03 19:11:23 +0530

    Updates

---
+++ precache-manifest.63c07c4464e39d4140344dc338d19e9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb0d43e6217a0e2a34f5c16bedf5f9b",
+    "url": "/myfirstreact/index.html"
+  },
+  {
+    "revision": "33589cfdd29d96724cac",
+    "url": "/myfirstreact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0b528a33096c6057ead1",
+    "url": "/myfirstreact/static/css/main.d98786d5.chunk.css"
+  },
+  {
+    "revision": "33589cfdd29d96724cac",
+    "url": "/myfirstreact/static/js/2.55053b68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myfirstreact/static/js/2.55053b68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b528a33096c6057ead1",
+    "url": "/myfirstreact/static/js/main.e5e7ee74.chunk.js"
+  },
+  {
+    "revision": "0f7b0231c4cb1cf2557a",
+    "url": "/myfirstreact/static/js/runtime-main.a45b9669.js"
+  }
+]);
